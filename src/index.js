b335db682d4514a312f6ca3ea9342fc597e12528
nixtoon <127158636+nixtoon@users.noreply.github.com>
2023-10-18 19:22:33 -0300

    Comprometiendo cambios en el submódulo

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const app = express()
-const path = require('path')
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
 const ejs = require('ejs');
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 
-// ejs
-app.set('view engine', '.ejs');
+// Configuración para EJS
+app.set('view engine', 'ejs');
 
 // Middleware
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
+
+// Middleware para CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Especifica los métodos permitidos
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Especifica los encabezados permitidos
+  next();
+});
 
 // routes
-app.use('/api',require('./routes/app'));
+app.use('/api', require('./routes/app'));
 
-// public 
+// public
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), () => {
-  console.log('listening on port: ', app.get('port'))
+  console.log('Escuchando en el puerto: ', app.get('port'));
 });
